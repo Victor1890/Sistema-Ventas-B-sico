877dc62c0f532578840c090d144cfa04554e4682
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-08-03 21:01:05 -0400

    Genial

--- main.c
+++ main.c
@@ -15,10 +15,26 @@ void main()
     SetConsoleTitle("Proyecto final de Eimy Corcino - 20186245");
     printf("Antes de usar este sotfware, primero deber%c ingresar los titulos de los libros al sistema", 160);
     printf("\nen la opci%cn del bibliotecario y luego as%c podra usar el sistema de biblioteca en la opci%cn de estudiantes",162, 161, 162);
-    printf("\nQue tenga un lindo d%ca :)",161);
+    printf("\nQue tenga un lindo d%ca :)\n",161);
     getchar();
     Roles_menu(libros);
 }
+
+//void Advertencia()
+//{
+//    printf("                       __\n");
+//    printf("                     .'  '.");
+//    printf("                 _.-'/  |  \\");
+//    printf("    ,        _.-\"  ,|  /  0 `-.");
+//    printf("    |\\    .-\"       `--\"\"-.__.'=====================-,");
+//    printf("    \\ '-'`        .___.--._)============================|");
+//    printf("     \\            .'         |                          |");
+//    printf("      |     /,_.-'            |        Advertencia       |");
+//    printf("    _/   _.'(                 |            de            |");
+//    printf("   /  ,-' \\  \\              |      Error de Memoria    |");
+//    printf("   \\  \\    `-'              |                          |");
+//    printf("    `-'                       '--------------------------'");
+//}
 
 void Roles_menu(struct Libros *libros)
 {
